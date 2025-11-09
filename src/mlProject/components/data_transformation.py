5031b1a91710038b36a6b22971bce4d49e46b197
sivaaitechtalks <siva.aitechtalks@gmail.com>
2025-11-09 21:46:51 +0530

    Data Transformation added

---
+++ src/mlProject/components/data_transformation.py
@@ -0,0 +1,24 @@
+import os
+from mlProject import logger
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from mlProject.entity.config_entity import DataTransformationConfig
+
+class DataTransformation:
+    def __init__(self, config: DataTransformationConfig):
+        self.config = config
+    
+    #train test split (we can do all the feature engineering, eda, transformation, scaling stuff here!)
+
+    def train_test_splitting(self):
+        data = pd.read_csv(self.config.data_path)
+
+        train, test = train_test_split(data)
+
+        train.to_csv(os.path.join(self.config.root_dir, "train.csv"), index=False)
+        test.to_csv(os.path.join(self.config.root_dir, "test.csv"), index=False)
+
+        logger.info("Splitted data into training and test data")
+        logger.info(train.shape)
+        logger.info(test.shape)
+
